ace85359e152cf5124439fc8c57e2a3920ccacfa
egressyk <egressyk@gmail.com>
2018-07-10 13:42:24 +0200

    Controller getAll and addPresent route

--- src/main/java/com/codecool/present/microservice_present/controller/PresentController.java
+++ src/main/java/com/codecool/present/microservice_present/controller/PresentController.java
@@ -1,4 +1,42 @@
 package com.codecool.present.microservice_present.controller;
 
+import com.codecool.present.microservice_present.model.Present;
+import com.codecool.present.microservice_present.service.PresentService;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+
+//Present	Add	/present	POST	Present	Present
+//Present	Remove	/present/{id}	DELETE	ID	HTTP OK
+//Present	Modify	/present/{id}	PUT	ID, Present	Present
+
+//Present	Get present by ID	/present/{id}	GET	ID	Present
+
+@Controller
 public class PresentController {
+
+    PresentService presentService;
+
+    public PresentController(PresentService presentService) {
+        this.presentService = presentService;
+    }
+
+    @GetMapping("/present")
+    public List<Present> getAllPresents(){
+        return presentService.getAllPresents();
+    }
+
+    @PostMapping("/present")
+    public Present addPresent(@RequestBody Present present) {
+        presentService.addPresent(present);
+        return present;
+    }
+
+
+
 }
--- src/main/java/com/codecool/present/microservice_present/service/PresentService.java
+++ src/main/java/com/codecool/present/microservice_present/service/PresentService.java
@@ -1,4 +1,7 @@
 package com.codecool.present.microservice_present.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PresentService {
 }
