cf0c43b8c0931a11ce15fd5818e6828c8577d8cb
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-07-10 12:49:35 +0200

    Create present model

--- src/main/java/com/codecool/present/microservice_present/model/Present.java
+++ src/main/java/com/codecool/present/microservice_present/model/Present.java
@@ -1,4 +1,109 @@
 package com.codecool.present.microservice_present.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 public class Present {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String name;
+    String description;
+    int price;
+    String imageUrl;
+    String category;
+    boolean available;
+    long userId;
+    LocalDateTime creation;
+
+
+    public Present() {
+    }
+
+    public Present(String name, String description, int price, String imageUrl, String category, long userId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        available = true;
+        this.userId = userId;
+        creation = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreation() {
+        return creation;
+    }
+
+    public void setCreation(LocalDateTime creation) {
+        this.creation = creation;
+    }
 }
