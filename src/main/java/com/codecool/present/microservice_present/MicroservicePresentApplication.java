e31e6c6863cad92dd9542b96172fa2da4c75df1e
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-07-10 09:42:37 +0200

    Skeleton project

---
+++ src/main/java/com/codecool/present/microservice_present/MicroservicePresentApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.present.microservice_present;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePresentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePresentApplication.class, args);
+	}
+}
